697660548298e236a9d784c91e9f2d257163ae8c
effxiikc <effxiikc@da0ad1ec-c5fd-45d5-9aa8-92a9e25fc676>
2011-08-09 16:26:13 +0000

    changed name from .m to .mm

---
+++ trunk/Classes/GameObjects/Collectables/CollectablesCache.mm
@@ -0,0 +1,23 @@
+//
+//  CollectablesCache.m
+//  mushroom
+//
+//  Created by Kelvin on 8/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CollectablesCache.h"
+
+@implementation CollectablesCache
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
