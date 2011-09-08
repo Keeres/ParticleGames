e8c594e217ee2fb0c641f9c38fb4e2a0076731c5
particlegames <particlegames@da0ad1ec-c5fd-45d5-9aa8-92a9e25fc676>
2011-09-08 04:01:39 +0000

    Removed buggy project files

--- trunk/Doomies/Doomies With Buddies/Classes/Box2D/Box2DHelpers.h
+++
@@ -1,7 +0,0 @@
-#import "Box2D.h"
-#import "CommonProtocols.h"
-
-bool isBodyCollidingWithObjectType(b2Body *body, 
-                                   GameObjectType objectType);
-
-void setBodyMask(b2Body *body, uint16 maskBits);
