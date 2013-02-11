e4be118a15690c4754bb1b56ad94bfb148e2a312
particlegames <particlegames@da0ad1ec-c5fd-45d5-9aa8-92a9e25fc676>
2013-02-11 04:52:05 +0000

    Updates to use PacketHandler, Packet, GameStates for data receiving and sending Server will now respond to PLAYER_INIT, PLAYER_HISTORY, REQUEST_PLAYER_INFO. Server will now send out PLAYER_CONNECTED Temporarily disabled SQL code

--- Geo/trunk/Net/Server.py
+++ Geo/trunk/Net/Server.py
@@ -1,184 +1,191 @@
-import time
-import datetime
+import sqlite3
+from time import time
+from GameStates import GameStates 
+from struct import pack, unpack
+from PacketHandler import PacketHandler
+from Packet import Packet
 from twisted.internet.protocol import Factory, Protocol
 from twisted.internet import reactor
-from struct import pack, unpack
-from zlib import crc32
-import sqlite3
 
-PACKET_HEADER_SIZE = 16
-PACKET_KEYWORD     = 'PARTICLE'
+ENOUGH_PACKET_DATA = 24
 
-class Player:
+class Player(object):
     def __init__(self, protocol, playerId, alias):
         self.protocol = protocol
         self.playerId = playerId
         self.alias = alias
         self.match = None
-        self.email = None
+        self.lastLogin = time()
+        
+        # TODO: Look up server history to see player status
+        self.protocol.sendPacket(self.protocol.gameState.playerConnected('PlayerConnected:Success'))
+        
+        print 'Player Connected: ' + playerId
+        
+
+    def processState(self, state, data):
+        if (state == GameStates.PLAYER_HISTORY):
+            # TODO: Need to give more player history
+            self.protocol.sendPacket(self.protocol.gameState.playerHistory("LastLogin:{0}".format(self.lastLogin)))
+
+        if (state == GameStates.REQUEST_PLAYER_INFO):
+            self.protocol.sendPacket(self.protocol.gameState.requestPlayerInfo("Player:{0}".format(self)))
+    
     
     def __repr__(self):
-        return "%s:%s" % (self.playerId, self.email)
+        return "%s:%s" % (self.playerId, self.alias)
 
 
 class Factory(Factory):
     def __init__(self):
         self.protocol = Protocol
         self.players = []
-    
+
+
     def connectionLost(self, protocol):
         for existingPlayer in self.players:
             # Search through list of players to find who disconnected
             # Remove player from list of players
             if existingPlayer.protocol == protocol:
                 existingPlayer.protocol = None
-    
+
+
     def playerConnected(self, protocol, playerId, alias, continueMatch):
         for existingPlayer in self.players:
             if existingPlayer.playerId == playerId:
                 existingPlayer.protocol = protocol
                 protocol.player = existingPlayer
-        
-        
+
         newPlayer = Player(protocol, playerId, alias)
         protocol.player = newPlayer
         self.players.append(newPlayer)
 
 
 class Protocol(Protocol):
-    
     def __init__(self):
         self.inBuffer = ""
         self.player = None
-    
+        self.packetHandler = PacketHandler()
+        self.gameState = GameStates()
+
+
     def log(self, message):
         if (self.player):
             print "%s: %s" % (self.player.alias, message)
         else:
             print "%s: %s" % (self, message)
-    
+
+
     def connectionMade(self):
         self.factory.clients.append(self)
         self.log("Connection made.")
         self.log("Clients List: %s" % str(self.factory.clients))
-        self.sendPlayerConnectAck()
-    
-    
+
+
     def connectionLost(self, reason):
         self.factory.clients.remove(self)
         self.log("Connection lost: %s" % str(reason))
-    
-    def sendDataEcho(self, data):
-        self.send(data)
-        print 'Echo Packet'
-    
-    def formatData(self, gameState, data):
-        timestamp = 1234
-        packetCounter = 0
-        packetSize = len(data)
-        
-        # TBD Calculate CRC for whole data packet
-        crc = crc32(data)
-        
-        packedData = pack('! 8s l Q B B H ' + str(packetSize) + 's', PACKET_KEYWORD, crc, timestamp, gameState, packetCounter, packetSize, data)
-        
-        self.send(packedData)
-    
-    def processPacket(self, crc, timestamp, gameState, packetCounter, dataSize, data):
-        self.DEBUG_printPacket(crc, timestamp, gameState, packetCounter, dataSize, data)
-        
-        print 'Sending Packet Echo Response'
-        #self.formatData(gameState, data)
-        #self.formatData(gameState + 1, 'Response-' + data)
-        
-        if gameState == 1: # PLAYER_INIT
-            print 'INFORMATION: Sending Player Init Ack'
-            self.sendPlayerInitAck(data)
-        
-        if gameState == 5: # PLAYER_HISTORY
-            self.sendPlayerHistory()
 
-        if gameState == 207: # REQUEST_SERVER_TERRITORIES
-            self.sendServerTerritories()
-    
-    
+
+    def processPacket(self, packet):
+
+        if (self.player == None):
+            if (packet.GameState == GameStates.PLAYER_INIT):
+                # Initialize player
+                self.factory.playerConnected(self, packet.Data, '', False)
+                self.sendPacket(self.gameState.playerInit('PlayerInit:Success'))
+            else:
+                # Client trying to send invalid packets before player initalized
+                self.sendPacket(self.gameState.playerInit('PlayerInit:Fail'))
+
+        else:
+            # Let player class determine player and game actions
+            self.player.processState(packet.GameState, packet.Data)
+            
+            #if (packet.GameState >= GameStates.REQUEST_SERVER_STATS)
+                # Process server related packets
+            
+        # if gameState == 1:  # PLAYER_INIT
+            # print 'INFORMATION: Sending Player Init Ack'
+            # self.sendPlayerInitAck(data)
+# 
+        # if gameState == 5:  # PLAYER_HISTORY
+            # self.sendPlayerHistory()
+# 
+        # if gameState == 207:  # REQUEST_SERVER_TERRITORIES
+            # self.sendServerTerritories()
+
+            
     def dataReceived(self, data):
-        
         self.inBuffer = self.inBuffer + data
-        
-        print 'length:      :'  + str(len(self.inBuffer))
-        
+
         # Check if there is enough data to process
-        while (len(self.inBuffer) >= PACKET_HEADER_SIZE + 8):
+        while (len(self.inBuffer) >= ENOUGH_PACKET_DATA):
+
+            packetPos = self.packetHandler.findNext(self.inBuffer)
             
-            packetStart = self.inBuffer.find(PACKET_KEYWORD)
-            
-            if (packetStart > -1):
-                crc, timestamp, gameState, packetCounter, dataSize = unpack('! l Q B B H', self.inBuffer[packetStart + 8: packetStart + 8 + PACKET_HEADER_SIZE])
-                
-                self.inBuffer = self.inBuffer[packetStart + 8 + PACKET_HEADER_SIZE :]
-                
-                # Check if we received enough data.
-                if (len(self.inBuffer) >= dataSize):
-                    packetData = self.inBuffer[: dataSize]
+            if (packetPos > -1):
+                dataExtracted = self.packetHandler.extract(self.inBuffer[packetPos :])
+                if (dataExtracted > -1):
+                    self.inBuffer = self.inBuffer[packetPos + dataExtracted :]
+                    self.processPacket(self.packetHandler.get())
+                    print 'Received Packet --'
+                    self.packetHandler.DEBUG_PRINT_PACKET()
                     
-                    # Extracted data from inBuffer, move data down the pipe
-                    self.inBuffer = self.inBuffer[dataSize :]
+
+    def sendPacket(self, packet):
+        self.transport.write(self.packetHandler.getPackedDataFromPacket(packet))
+        
+
+    def send(self, message):
+        self.transport.write(message)
+
+
+
                     
-                    # Process data
-                    self.processPacket(crc, timestamp, gameState, packetCounter, dataSize, packetData)
-    
-    
-    def DEBUG_printPacket(self, crc, timestamp, gameState, packetCounter, dataSize, packetData):
-        print 'Data Received - Packet Dump'
-        print 'CRC            :: ' + str(crc)
-        print 'Timestamp      :: ' + str(timestamp)
-        print 'Game State     :: ' + str(gameState)
-        print 'Packet Counter :: ' + str(packetCounter)
-        print 'Data Size      :: ' + str(dataSize)
-        print 'Data           :: ' + packetData
-    
+
+
     def sendPlayerInitAck(self, data):
         print 'sendPlayerInitAck: ' + data
         cursor.execute("""CREATE TABLE IF NOT EXISTS [PLAYERSTATS] ([Index] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, [ID] VARCHAR(32) UNIQUE NOT NULL, [NAME] TEXT NOT NULL, [TYPE] TEXT, [PICTURE] TEXT, [DATA] TEXT)""")
         cursor.execute("""CREATE TABLE IF NOT EXISTS [PLAYERCHALLENGERS] ([Index] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, [ID] VARCHAR(32) UNIQUE NOT NULL, [NAME] TEXT NOT NULL, [EMAIL] TEXT, [PICTURE] TEXT, [WIN] INTEGER, [LOSS] INTEGER,[MATCHSTARTED] TEXT, [LASTPLAYED] TEXT)""")
         data = 'Player Init Ack: ' + data
         timestamp = 2345
-        gameState = 1 # Player_Init
+        gameState = 1  # Player_Init
         packetCounter = 0
         packetSize = len(data)
         crc = crc32(data)
         packedData = pack('! 8s l Q B B H ' + str(packetSize) + 's', PACKET_KEYWORD, crc, timestamp, gameState, packetCounter, packetSize, data)
         self.send(packedData)
-    
+
     def sendPlayerConnectAck(self):
         data = "Player Connect Ack"
         timestamp = 2345
-        gameState = 2 # Player_Connected
+        gameState = 2  # Player_Connected
         packetCounter = 0
         packetSize = len(data)
         crc = crc32(data)
         packedData = pack('! 8s l Q B B H ' + str(packetSize) + 's', PACKET_KEYWORD, crc, timestamp, gameState, packetCounter, packetSize, data)
         self.send(packedData)
-    
-    
+
+
     def sendPlayerHistory(self):
         print 'sendPlayerHistory'
         cursor.execute("""SELECT * FROM PLAYERCHALLENGES""")
         data = ""
-        
+
         for row in cursor:
             data += '('
             for item in row:
-                if row[len(row)-1] == item:
+                if row[len(row) - 1] == item:
                     data += str(item)
                 else:
                     data += str(item) + ','
             data += ')'
-        
-        #data = "Player History Ack"
+
+        # data = "Player History Ack"
         timestamp = 2345
-        gameState = 5 # Player_History
+        gameState = 5  # Player_History
         packetCounter = 0
         packetSize = len(data)
         crc = crc32(data)
@@ -193,29 +200,28 @@ class Protocol(Protocol):
         for row in cursor:
             data += '('
             for item in row:
-                if row[len(row)-1] == item:
+                if row[len(row) - 1] == item:
                     data += str(item)
                 else:
                     data += str(item) + ','
             data += ')'
 
         timestamp = 3456
-        gameState = 207 # Request_Server_Territories
+        gameState = 207  # Request_Server_Territories
         packetCounter = 0
         packetSize = len(data)
         crc = crc32(data)
         packedData = pack('! 8s l Q B B H ' + str(packetSize) + 's', PACKET_KEYWORD, crc, timestamp, gameState, packetCounter, packetSize, data)
         self.send(packedData)
-    
 
-    def send(self, message):
-        self.transport.write(message)
 
-conn = sqlite3.connect("/Users/kelvin/Documents/iPhone/Projects/Current/SVN/GeoQuestPortrait/Net/GeoQuestServerDB.sqlite")
-cursor = conn.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS [AllPlayers] ([Index] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, [ID] INTEGER UNIQUE NOT NULL, [Name] TEXT NOT NULL, [Email] TEXT UNIQUE NOT NULL, [Picture] TEXT)""")
-conn.commit()
-print "Database loaded"
+
+
+# conn = sqlite3.connect("/Users/kelvin/Documents/iPhone/Projects/Current/SVN/GeoQuestPortrait/Net/GeoQuestServerDB.sqlite")
+# cursor = conn.cursor()
+# cursor.execute("""CREATE TABLE IF NOT EXISTS [AllPlayers] ([Index] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, [ID] INTEGER UNIQUE NOT NULL, [Name] TEXT NOT NULL, [Email] TEXT UNIQUE NOT NULL, [Picture] TEXT)""")
+# conn.commit()
+# print "Database loaded"
 
 factory = Factory()
 factory.clients = []
